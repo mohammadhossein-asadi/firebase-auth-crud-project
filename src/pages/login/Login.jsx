7a665e487f1702488e713924f788aa57379f8067
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-07-02 18:52:23 +0430

    added style login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,15 @@
-import "./login.scss"
+import "./login.scss";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login">
+      <form>
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
