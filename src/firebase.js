3ec67c776e8caa4095ac6d928eb99ef84ef3f092
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-07-02 18:41:24 +0430

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "fir-project-5f0ac.firebaseapp.com",
+  projectId: "fir-project-5f0ac",
+  storageBucket: "fir-project-5f0ac.appspot.com",
+  messagingSenderId: "184978544281",
+  appId: "1:184978544281:web:058995e6cedeea58090a08",
+};
+
+const app = initializeApp(firebaseConfig);
