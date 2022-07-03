63ffb3c5e7a116bdb524b2b6193080d25db608d8
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-07-03 22:32:33 +0430

    added setDoc

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -3,9 +3,20 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import { useState } from "react";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../../firebase";
 
 const New = ({ inputs, title }) => {
   const [file, setFile] = useState("");
+
+  const handleAdd = async (e) => {
+    e.preventDefault();
+    await setDoc(doc(db, "cities", "LA"), {
+      name: "Los Angeles",
+      state: "CA",
+      country: "USA",
+    });
+  };
 
   return (
     <div className="new">
@@ -27,7 +38,7 @@ const New = ({ inputs, title }) => {
             />
           </div>
           <div className="right">
-            <form>
+            <form onSubmit={handleAdd}>
               <div className="formInput">
                 <label htmlFor="file">
                   Image: <DriveFolderUploadOutlinedIcon className="icon" />
@@ -46,7 +57,7 @@ const New = ({ inputs, title }) => {
                   <input type={input.type} placeholder={input.placeholder} />
                 </div>
               ))}
-              <button>Send</button>
+              <button type="submit">Send</button>
             </form>
           </div>
         </div>
